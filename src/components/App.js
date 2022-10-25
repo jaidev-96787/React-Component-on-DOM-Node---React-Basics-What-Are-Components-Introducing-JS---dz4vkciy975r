f4caed1fbd294496a8955375dc3e2a216448b26c
Jaidev <jaidevsah007@gmail.com>
2022-10-25 12:36:16 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-
-export default function App(){
-    return(<>
-       <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> 
-    </>)
+import '../styles/App.css';
+const App = () => {
+  return (
+    <p>I am learning React. My life is getting better</p>
+  )
 }
+
+export default App;
 
 
 
